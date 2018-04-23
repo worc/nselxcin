f46964fbef2a00411647fc6f25879978ac312def
worc <curtis.s.mcallister@gmail.com>
2018-04-23 13:10:17 -0700

    change Insert functions to static methods

--- db/Insert.js
+++ db/Insert.js
@@ -14,41 +14,39 @@ const logOutcome = function(resolve, reject) {
     };
 };
 
+const db = new sqlite3.Database(__dirname + '/nselxcin.db', logOutcome(() => 'connected'), () => 'failed');
+
+function insert(sql, params) {
+    return new Promise((resolve, reject) => {
+        db.run(sql, params, logOutcome(resolve, reject));
+    });
+}
+
 export default class Insert {
-    constructor() {
-        this.db = new sqlite3.Database(__dirname + '/nselxcin.db', logOutcome(() => 'connected'), () => 'failed');
-    }
-
-    insert(sql, params) {
-        return new Promise((resolve, reject) => {
-            this.db.run(sql, params, logOutcome(resolve, reject));
-        });
-    }
-
-    intoWorkbooks({ title = '', subtitle = '', authors = '', version = '', edition = '' }) {
+    static intoWorkbooks({ title = '', subtitle = '', authors = '', version = '', edition = '' }) {
         console.log('title:', title);
         console.log('subtitle:', subtitle);
         const sql = `insert into Workbooks(title, subtitle, authors, version, edition) values(?, ?, ?, ?, ?)`;
         const params = [title, subtitle, authors, version, edition];
         console.log(params);
-        return this.insert(sql, params);
+        return insert(sql, params);
     }
 
-    intoPhrases({ salish = '', english = '', audioUrl = '' }) {
+    static intoPhrases({ salish = '', english = '', audioUrl = '' }) {
         const sql = `insert into Phrases(salish, english, audioUrl) values(?, ?, ?)`;
         const params = [salish, english, audioUrl];
-        return this.insert(sql, params);
+        return insert(sql, params);
     }
 
-    intoLessons({ workbookId, lessonNumber = 0, lessonName = '' }) {
+    static intoLessons({ workbookId, lessonNumber = 0, lessonName = '' }) {
         const sql = `insert into Lessons(workbookId, lessonNumber, lessonName) values(?, ?, ?)`;
         const params = [ workbookId, lessonNumber, lessonName ];
-        return this.insert(sql, params);
+        return insert(sql, params);
     }
 
-    intoLessonPhrases({ lessonId, phraseId, viewOrder }) {
+    static intoLessonPhrases({ lessonId, phraseId, viewOrder }) {
         const sql = `insert into LessonPhrases(lessonId, phraseId, viewOrder) values(?, ?, ?)`;
         const params = [lessonId, phraseId, viewOrder];
-        return this.insert(sql, params);
+        return insert(sql, params);
     }
 }
