70cd382cec2b1b16a13eb0133bfa10b040039f52
worc <curtis.s.mcallister@gmail.com>
2018-04-23 12:28:56 -0700

    fix select to retrieve data

--- db/Select.js
+++ db/Select.js
@@ -1,21 +1,27 @@
+import sqlite3 from "sqlite3";
+
 const logOutcome = function(resolve, reject) {
     // can't use fat-arrow, db.run will put successful
     // data and message on the function context--"this"
-    return function(error) {
+    return function(error, rows) {
         if(error) {
             console.error(error.message);
             reject(error.message);
         } else {
             console.log('success:', this);
-            resolve(this);
+            resolve(rows);
         }
     };
 };
 
 export default class Select {
+    constructor() {
+        this.db = new sqlite3.Database(__dirname + '/nselxcin.db', logOutcome(() => 'connected'), () => 'failed');
+    }
+
     select(sql, params) {
         return new Promise((resolve, reject) => {
-            this.db.run(sql, params, logOutcome(resolve, reject));
+            this.db.all(sql, params, logOutcome(resolve, reject));
         });
     }
 
