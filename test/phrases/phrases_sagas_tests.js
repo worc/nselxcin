751756894beaae7973e48365fac00e6e6c1db80f
worc <curtis.s.mcallister@gmail.com>
2018-07-27 19:11:20 -0700

    add test for dispatching the new phrase

--- test/phrases/phrases_sagas_tests.js
+++ test/phrases/phrases_sagas_tests.js
@@ -5,21 +5,22 @@ import { call, put, all } from 'redux-saga/effects'
 
 import * as sagas from '../../src/features/phrases/phrases_sagas'
 import * as api from '../../src/features/phrases/phrases_requests'
+import * as action from '../../src/features/phrases/phrases_actions'
 
 describe('phrases sagas', () => {
     describe('addPhrase saga', () => {
-        beforeEach(() => {
-            sinon.stub(api, 'addPhrase').callsFake(() => {
-                return {
-                    id: 0,
-                    english: 'english',
-                    salish: 'salish',
-                    audioUrl: 'url'
-                }
-            })
+        const mockResponse = {
+            id: 0,
+            english: 'english',
+            salish: 'salish',
+            audioUrl: 'url'
+        }
+
+        before(() => {
+            sinon.stub(api, 'addPhrase').callsFake(() => mockResponse)
         })
 
-        afterEach(() => {
+        after(() => {
             api.addPhrase.reset()
         })
 
@@ -35,6 +36,23 @@ describe('phrases sagas', () => {
             assert.deepEqual(
                 gen.next().value,
                 call(api.addPhrase, mockMessage.english, mockMessage.salish, mockMessage.audioUrl)
+            )
+        })
+
+        it('dispatches the new phrase with the phrase id', () => {
+            const mockMessage = {
+                english: 'english',
+                salish: 'salish',
+                audioUrl: 'audioUrl'
+            }
+
+            const gen = sagas.addPhrase(mockMessage)
+
+            gen.next()
+
+            assert.deepEqual(
+                gen.next(mockResponse).value,
+                put({ type: action.RECEIVE_PHRASE, phrase: mockResponse })
             )
         })
     })
