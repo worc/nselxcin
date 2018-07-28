0f8df90ef50836bf039c0ace1fa2c9a7f990397c
worc <curtis.s.mcallister@gmail.com>
2018-07-27 20:12:58 -0700

    remove dead code

--- db/transactions/Insert.js
+++ db/transactions/Insert.js
@@ -1,6 +1,6 @@
 import db from '../db';
 
-const logOutcome = function(resolve, reject, params) {
+const logOutcome = function(resolve, reject) {
     // can't use fat-arrow, db.run will put successful
     // data and message on the function context--"this"
     return function(error) {
