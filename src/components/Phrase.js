7595fcf823c0cdde65564abbcc7ade08e864cd79
worc <curtis.s.mcallister@gmail.com>
2018-11-03 14:51:36 -0700

    stub out phrase props and styling

--- src/components/Phrase.js
+++ src/components/Phrase.js
@@ -1,3 +1,23 @@
 import React from 'react'
+import styled from 'styled-components'
 
-export default () => <h1>hello world</h1>
+export default ({ salish, english, url }) => (
+    <PhraseRow>
+        <StyledDiv>{ salish }</StyledDiv>
+        <StyledDiv>{ english }</StyledDiv>
+        <StyledDiv><audio controls src={ url }/></StyledDiv>
+    </PhraseRow>
+)
+
+const PhraseRow = styled.div`
+    border: 1px solid #bad;
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-between;
+    align-items: center;
+`
+
+const StyledDiv = styled.div`
+    border: 1px solid #ace;
+    flex: 0 0 auto;
+`
--- src/components/Phrase.story.js
+++ src/components/Phrase.story.js
@@ -2,5 +2,11 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import Phrase from './Phrase'
 
+const testProps = {
+    salish: 'salish goes here',
+    english: 'english translation',
+    url: 'https://soundbible.com/mp3/Tyrannosaurus%20Rex%20Roar-SoundBible.com-807702404.mp3'
+}
+
 storiesOf('Phrase', module)
-    .add('hello world', () => <Phrase/>)
+    .add('hello world', () => <Phrase { ...testProps } />)
