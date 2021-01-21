0caedb9a19152c3414fdf1490057adab6c97b740
worc <curtis.s.mcallister@gmail.com>
2021-01-21 14:46:39 -0800

    add test get, insert, delete to admin app

--- admin/app.js
+++ admin/app.js
@@ -1,21 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function testPost() {
-  console.log('posting...')
-  // axios.post('/api/workbook', {
-  //   title: 'title',
-  //   subtitle: 'subtitle',
-  //   authors: ['first', 'second'],
-  //   edition: 'edition',
-  //   version: 'version number',
-  // }).then(res => console.log(res)).catch(err => console.error(err))
+  console.log('posting... hopefully...')
+  axios.post('/api/workbook', {
+    title: 'title',
+    subtitle: 'subtitle',
+    authors: 'just a plain string',
+    edition: 'edition',
+    version: 'version number',
+  }).then(res => console.log(res)).catch(err => console.error(err))
+}
+
+function testDelete(workbook_id) {
+  console.log('deleting...')
+  axios.delete(`/api/workbook/${workbook_id}`).then(res => console.log(res)).catch(err => console.error(err))
 }
 
 export default function () {
+  const [workbooks, setWorkbooks] = useState([])
+
+  useEffect(() => {
+    axios.get('/api/workbooks').then(response => {
+      console.log(response)
+      setWorkbooks(response.data)
+    })
+  }, [])
+
   return (
     <div>
       <h1>hello world</h1>
       <div style={{border: '1px solid #ace'}} onClick={testPost}>test post request</div>
+      <div>
+        { workbooks.map(workbook => (
+          <li key={workbook.workbook_id}>
+            <div>{ workbook.title }</div>
+            <div>{ workbook.subtitle }</div>
+            <div>{ workbook.authors }</div>
+            <div>{ workbook.edition }</div>
+            <div>{ workbook.version }</div>
+            <div style={{ border: '2px solid red'}} onClick={() => testDelete(workbook.workbook_id)}>DELETE</div>
+          </li>
+        ))}
+      </div>
     </div>
   )
 }
