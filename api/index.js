412bbeadc95f3ee4dd8b1966a39be716d2ec084a
worc <curtis.s.mcallister@gmail.com>
2021-01-21 13:53:15 -0800

    add json parser to admin app workbook post route

--- api/index.js
+++ api/index.js
@@ -1,8 +1,16 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+
 const router = express.Router()
+const jsonParser = bodyParser.json()
 
 router.get('/', (req, res) => {
   res.send('api root')
+})
+
+router.post('/workbook', jsonParser, (req, res) => {
+  console.log(req.body)
+  res.send('ok')
 })
 
 export default router
