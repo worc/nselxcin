e58682536182bfb465390fd44d91bc4a937e61be
worc <curtis.s.mcallister@gmail.com>
2021-01-29 15:14:23 -0800

    copy audio_id to phrases

---
+++ postgres/migrations/10_copy_audio_id_to_phrases.js
@@ -0,0 +1,35 @@
+const { Client } = require('pg')
+
+async function setup ({ log }) {
+  const client = new Client({
+    database: 'nselxcin-test',
+    host: 'localhost',
+    password: 'salish',
+    port: 5432,
+    user: 'nselxcin',
+  })
+
+  function logResponse (err, res) {
+    if (log && res) {
+      console.log(res)
+    }
+  }
+
+  try {
+    client.connect()
+
+    await client.query(`
+      UPDATE phrases
+      SET audio_id = audio.audio_id
+      FROM audio
+      WHERE audio.phrase_id = phrases.phrase_id
+    `).then(logResponse)
+
+  } catch (error) {
+    console.error(error)
+  }
+
+  client.end()
+}
+
+setup({ log: true }).then(() => console.log('copy audio_id to phrases'))
