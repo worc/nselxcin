ce2bead522f17edf2e1ddc3b7d443ee8302e8de8
worc <curtis.s.mcallister@gmail.com>
2021-01-29 15:07:00 -0800

    copy salish_audio and phrase_id to audio

---
+++ postgres/migrations/9_copy_phrase_audio_to_audio_file.js
@@ -0,0 +1,34 @@
+const { Client } = require('pg')
+
+async function setup ({ log }) {
+  const client = new Client({
+    database: 'nselxcin-test',
+    host: 'localhost',
+    password: 'salish',
+    port: 5432,
+    user: 'nselxcin',
+  })
+
+  function logResponse (err, res) {
+    if (log && res) {
+      console.log(res)
+    }
+  }
+
+  try {
+    client.connect()
+
+    await client.query(`
+      INSERT INTO audio(file, phrase_id)
+      SELECT salish_audio, phrase_id FROM phrases
+      WHERE phrases.audio_id IS NULL
+    `).then(logResponse)
+
+  } catch (error) {
+    console.error(error)
+  }
+
+  client.end()
+}
+
+setup({ log: true }).then(() => console.log('copy phrases.salish_audio, phrases.phrase_id to audio'))
