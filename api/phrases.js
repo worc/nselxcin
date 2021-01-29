c44f3a9e654bcef829338fc924310c178329257d
worc <curtis.s.mcallister@gmail.com>
2021-01-29 15:23:54 -0800

    update phrases api to get audio data from the audio table

--- api/phrases.js
+++ api/phrases.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser'
 import query from './_query.js'
 
 async function getAllPhrases() {
-  const result = await query(`SELECT phrase_id, salish, english, octet_length(salish_audio) AS size FROM phrases`)
+  const result = await query(`SELECT phrases.phrase_id, salish, english, octet_length(file) AS size, filename FROM phrases JOIN audio ON phrases.audio_id = audio.audio_id`)
   return result.rows
 }
 
@@ -52,15 +52,15 @@ router.route('/phrase/:id')
 router.route('/phrase/:id/audio')
   .all(mpegParser)
   .get(async (req, res) => {
-    const text = `SELECT salish_audio, octet_length(salish_audio) AS size FROM phrases WHERE phrase_id = $1`
+    const text = `SELECT file, octet_length(file) AS size, filename FROM audio JOIN phrases ON phrases.audio_id = audio.audio_id WHERE phrases.phrase_id = $1`
     const values = [ req.params.id ]
     const result = await query(text, values)
-    const phrase_audio = result.rows[0]
+    const audio = result.rows[0]
 
     res.set('Content-Type', 'audio/mpeg')
-    res.set('Content-Length', `${phrase_audio.size}`)
+    res.set('Content-Length', `${audio.size}`)
 
-    res.send(phrase_audio.salish_audio)
+    res.send(audio.file)
   })
   .put(async (req, res) => {
     await addSalishAudioToPhrase(req.params.id, req.body)
