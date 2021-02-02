8faf73b639e809bedddd46745e32291dae9e20e2
worc <curtis.s.mcallister@gmail.com>
2021-02-02 04:48:53 -0800

    remove a level of abstraction by building the sql statements within the router callbacks
    my thinking here is if i ever want to test this, i don't want to test sql statements in isolation. i'd rather go to the trouble to stand up a bare test database and test server and then create tests that make network calls and check the state of the database. these statements and expected states are simple enough that i'm of the mind to just skip of a level of testing (if i ever even get around to it).

--- api/audio.js
+++ api/audio.js
@@ -1,36 +1,20 @@
 import express from 'express'
 import query from './_query.js'
 
-async function getOrphanedAudio() {
-  const text = `SELECT audio_id, filename FROM AUDIO WHERE NOT EXISTS (SELECT FROM phrases WHERE phrases.audio_id = audio.audio_id)`
-  const result = await query(text)
-  return result.rows
-}
-
-async function addAudio(file, filename, content_type) {
-  const text = `INSERT INTO audio(file, filename, content_type) VALUES($1, $2, $3) RETURNING audio_id`
-  const values = [file, filename, content_type]
-  const result = await query(text, values)
-  return result.rows
-}
-
-async function deleteAudio(id) {
-  const text = `DELETE FROM audio WHERE audio_id = $1`
-  const values = [id]
-  return await query(text, values)
-}
-
 const router = express.Router()
 
 router.route('/audio')
   .get(async (req, res) => {
-    const orphanedAudio = await getOrphanedAudio()
-    res.send(orphanedAudio)
+    const text = `SELECT audio_id, filename FROM AUDIO WHERE NOT EXISTS (SELECT FROM phrases WHERE phrases.audio_id = audio.audio_id)`
+    const result = await query(text)
+
+    res.send(result.rows)
   })
   .post(async (req, res) => {
-    const filename = req.get('Filename')
-    const content_type = req.get('Content-Type')
-    const result = await addAudio(req.body, filename, content_type)
+    const text = `INSERT INTO audio(file, filename, content_type) VALUES($1, $2, $3) RETURNING audio_id`
+    const values = [req.body, req.get('Filename'), req.get('Content-Type')]
+    const result = await query(text, values)
+
     res.send(result[0])
   })
 
@@ -47,8 +31,11 @@ router.route('/audio/:id')
     res.send(audio.file)
   })
   .delete(async (req, res) => {
-    await deleteAudio(req.params.id)
-    res.send('ok')
+    const text = `DELETE FROM audio WHERE audio_id = $1 RETURNING audio_id, filename, content_type`
+    const values = [req.params.id]
+    const result = query(text, values)
+
+    res.send(result.rows[0])
   })
 
 router.route('/audio/:id/metadata')
@@ -56,6 +43,7 @@ router.route('/audio/:id/metadata')
     const text = 'SELECT octet_length(file) AS size, filename, content_type FROM audio WHERE audio_id = $1'
     const values = [req.params.id]
     const result = await query(text, values)
+
     res.send(result.rows[0])
   })
 
--- api/phrases.js
+++ api/phrases.js
@@ -1,64 +1,54 @@
 import express from 'express'
 import query from './_query.js'
 
-async function getAllPhrases() {
-  const result = await query(`SELECT phrases.phrase_id, salish, english, octet_length(file) AS size, filename FROM phrases JOIN audio ON phrases.audio_id = audio.audio_id`)
-  return result.rows
-}
-
-async function insertPhrase(salish, english) {
-  const text = `INSERT INTO phrases(
-    salish,
-    english
-  ) VALUES($1, $2) RETURNING phrase_id`
-
-  const values = [ salish, english ]
-  const result = await query(text, values)
-  const phrase_id = result.rows[0].phrase_id
-  console.log(phrase_id)
-  return phrase_id
-}
-
-async function addSalishAudioToPhrase(phrase_id, audio, filename, content_type) {
-  const audioText = `INSERT INTO audio(file, filename, content_type) VALUES($1, $2, $3) RETURNING audio_id`
-  const audioValues = [audio, filename, content_type]
-  const audioResult = await query(audioText, audioValues)
-  const audioId = audioResult.rows[0].audio_id
-
-  const phrasesText = `UPDATE phrases SET audio_id = $2 WHERE phrase_id = $1`
-  const phrasesValues = [phrase_id, audioId]
-  const result = await query(phrasesText, phrasesValues)
-
-  return result.rows
-}
-
 const router = express.Router()
+
+router.route('/phrases')
+  .get(async (req, res) => {
+    const result = await query(`SELECT phrases.phrase_id, salish, english, octet_length(file) AS size, filename FROM phrases JOIN audio ON phrases.audio_id = audio.audio_id`)
+
+    res.send(result.rows)
+  })
+
+router.route('/phrase')
+  // TODO handle creating a new phrase with a pre-existing audio_id
+  // TODO with the 1:many relationship here, i think it would have to
+  // TODO be supplied from the client instead of looked up during the query
+  .post(async (req, res) => {
+    const text = 'INSERT INTO phrases(salish, english) VALUES($1, $2) RETURNING *'
+    const values = [req.body.salish, req.body.english]
+    const result = await query(text, values)
+
+    res.send(result.rows[0])
+  })
 
 router.route('/phrase/:id')
   .get(async (req, res) => {
-    const text = `SELECT phrase_id, salish, english, octet_length(salish_audio) AS size FROM phrases WHERE phrase_id = $1`
-    const values = [ req.params.id ]
+    const text = 'SELECT phrase_id, salish, english, octet_length(salish_audio) AS size FROM phrases WHERE phrase_id = $1'
+    const values = [req.params.id]
     const result = await query(text, values)
-    const phrase = result.rows[0]
-    res.send({ ...phrase })
+
+    res.send(result.rows[0])
   })
   .put(async (req, res) => {
     const text = 'UPDATE phrases SET salish = $1, english = $2 WHERE phrase_id = $3 RETURNING *'
     const values = [req.body.salish, req.body.english, req.params.id]
     const result = await query(text, values)
+
     res.send(result.rows[0])
   })
   .delete(async (req, res) => {
-    const text = `DELETE FROM phrases WHERE phrase_id = $1`
-    const values = [ req.params.id ]
-    await query(text, values)
-    res.send('ok')
+    const text = `DELETE FROM phrases WHERE phrase_id = $1 RETURNING *`
+    const values = [req.params.id]
+    const result = await query(text, values)
+
+    res.send(result.rows[0])
   })
 
 router.route('/phrase/:id/audio')
   .get(async (req, res) => {
     const text = `SELECT file, octet_length(file) AS size, filename FROM audio JOIN phrases ON phrases.audio_id = audio.audio_id WHERE phrases.phrase_id = $1`
-    const values = [ req.params.id ]
+    const values = [req.params.id]
     const result = await query(text, values)
     const audio = result.rows[0]
 
@@ -67,28 +57,29 @@ router.route('/phrase/:id/audio')
 
     res.send(audio.file)
   })
+  // TODO change to a POST? this is adding data
+  // TODO remove entirely, bulk upload has a different process where
+  // TODO the client handles the step-by-step procedure on its own to
+  // TODO add an audio file, then relay its ID to the phrases endpoint
   .put(async (req, res) => {
-    await addSalishAudioToPhrase(req.params.id, req.body)
-    res.send('ok')
+    const audioText = 'INSERT INTO audio(file, filename, content_type) VALUES($1, $2, $3) RETURNING audio_id'
+    const audioValues = [req.body, req.get('Filename'), req.get('Content-Type')]
+    const audioResult = await query(audioText, audioValues)
+    const audioId = audioResult.rows[0].audio_id
+
+    const phrasesText = 'UPDATE phrases SET audio_id = $2 WHERE phrase_id = $1 RETURNING *'
+    const phrasesValues = [req.params.id, audioId]
+    const result = await query(phrasesText, phrasesValues)
+
+    res.send(result.rows[0])
   })
 
 router.put('/phrase/:phrase_id/audio/:audio_id', async (req, res) => {
-  const text = `UPDATE phrases SET audio_id = $2 WHERE phrase_id = $1`
+  const text = `UPDATE phrases SET audio_id = $2 WHERE phrase_id = $1 RETURNING *`
   const values = [req.params.phrase_id, req.params.audio_id]
-  await query(text, values)
-  res.send('ok')
-})
+  const result = await query(text, values)
 
-router.post('/phrase', async (req, res) => {
-  console.log(req.body)
-  const phrase_id = await insertPhrase(req.body.salish, req.body.english)
-  res.send({ phrase_id })
+  res.send(result.rows[0])
 })
-
-router.get('/phrases', async (req, res) => {
-  const phrases = await getAllPhrases()
-  res.send(phrases)
-})
-
 
 export default router
