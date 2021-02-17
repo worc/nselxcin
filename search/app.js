bb774a2f1b6475b0d34212a4241b587058215bbf
worc <curtis.s.mcallister@gmail.com>
2021-02-17 14:44:21 -0800

    add search request to the front end, log results

--- search/app.js
+++ search/app.js
@@ -1,11 +1,29 @@
 import React from 'react'
+import axios from 'axios'
+import Input from '../components/input.js'
 
 export default function () {
-  function handleClick () {
-    console.log('clicked')
+  function handleSubmit (e) {
+    e.preventDefault()
+
+    console.log(e.target.query.value)
+
+    axios.get(`/api/search/phrases?q=${e.target.query.value}`).then(res => {
+      console.log(res.data)
+    }).catch(err => console.error(err))
   }
 
   return (
-    <h1 onClick={ handleClick }>hello world</h1>
+    <>
+      <h1>phrase search</h1>
+      <form id='search' onSubmit={ handleSubmit }>
+        <Input
+          id='query'
+          label='Search'
+          type='text'
+        />
+        <button>submit</button>
+      </form>
+    </>
   )
 }
