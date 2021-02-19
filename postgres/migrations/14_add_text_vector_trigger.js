66f1f90e74b4e8bb57890e45421f399c05c12906
worc <curtis.s.mcallister@gmail.com>
2021-02-19 12:51:17 -0800

    add trigger for updating and creating english vectors

---
+++ postgres/migrations/14_add_text_vector_trigger.js
@@ -0,0 +1,36 @@
+const { Client } = require('pg')
+
+async function setup ({ log }) {
+  const client = new Client({
+    database: 'nselxcin-test',
+    host: 'localhost',
+    password: 'salish',
+    port: 5432,
+    user: 'nselxcin',
+  })
+
+  function logResponse (err, res) {
+    if (log && res) {
+      console.log(res)
+    }
+  }
+
+  try {
+    client.connect()
+
+    await client.query(`
+      CREATE TRIGGER simple_english_vector_update BEFORE INSERT OR UPDATE
+      ON phrases FOR EACH ROW EXECUTE PROCEDURE
+      tsvector_update_trigger(
+        simple_english_text_vector, 'pg_catalog.simple', english
+      )
+    `).then(logResponse)
+
+  } catch (error) {
+    console.error(error)
+  }
+
+  client.end()
+}
+
+setup({ log: true }).then(() => console.log('add trigger for english text vectors'))
