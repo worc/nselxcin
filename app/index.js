e3bdad5a356f064461381af8bc03666b86e21c5c
worc <curtis.s.mcallister@gmail.com>
2021-02-26 14:28:30 -0800

    add aboriginal sans as the default font family, add a .salish class to mark words in the app in blue

--- app/index.js
+++ app/index.js
@@ -10,10 +10,15 @@ import Footer from '../components/footer.js'
 const GlobalStyles = createGlobalStyle`
   * {
     box-sizing: border-box;
+    font-family: "Aboriginal Sans", sans-serif;
   }
 
   h1, h2, h3, h4, h5, h6 {
     margin: 0;
+  }
+
+  .salish {
+    color: #2fb6d4;
   }
 `
 
